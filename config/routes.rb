ab7a5928c11ad4f9ca4f7394a1c40a2835798c91
sotakon <skon1221@gmail.com>
2021-05-03 16:59:19 +0900

    create: 1対多アソシエーション

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+end
